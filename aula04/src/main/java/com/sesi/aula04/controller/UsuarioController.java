61501aad5d3e0a77a824dd6fea9b6b97d11cfc2f
Hector2414 <hectorlaucher6@gmail.com>
2024-09-24 09:56:01 -0300

    Foi sincronizado com o banco de dados e criação da função de deletar 24-09-2024

--- aula04/src/main/java/com/sesi/aula04/controller/UsuarioController.java
+++ aula04/src/main/java/com/sesi/aula04/controller/UsuarioController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sesi.aula04.Repository.UsuarioRepository;
+import com.sesi.aula04.model.Usuario;
 
 
 
@@ -17,17 +21,36 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
+	//Exibe o Formulario
+	@GetMapping("/formulario")
+	public String mostrarFormulario(Model modelo) {
+		modelo.addAttribute("usuario", new Usuario());
+		return "formulario";
+	}
+	
+	
+	//Adicionar
+	@PostMapping("/salvarUsuario")
+	public String salvarUsuario(@ModelAttribute Usuario usuario) {
+		usuarioRepository.save(usuario);
+		return"redirect:/usuarios";
+	}
+	
+	//Listar 
 	@GetMapping("/usuarios")
 
 	public String listarUsuario(Model modelo) {
 	
-	 
-	 
-
-	 
 	 modelo.addAttribute("usuarios",usuarioRepository.findAll() );
 		
 		return "usuarios";
+	}
+	
+	//Remover
+	@GetMapping("/removerUsuario/{id}")
+	public String removerUsuario(@PathVariable int id) {
+		usuarioRepository.deleteById(id);
+		return"redirect:/usuarios";
 	}
 	
 }
